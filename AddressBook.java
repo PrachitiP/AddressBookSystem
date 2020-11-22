00cb2a706358b41b8f447880e1cb685f3e160e61
PrachitiP <68328603+PrachitiP@users.noreply.github.com>
2020-11-23 00:01:16 +0530

    Address Book System

--- AddressBook.java
+++ AddressBook.java
@@ -3,7 +3,7 @@ package BridgeLabz.AddressBook;
 public class AddressBook {
 
 	public static void main(String[] args) {
-		 System.out.println("Welcome to Address Book Program");
+		 System.out.println("Welcome to Address Book System");
 		 AddressBookMethods addressMethod = new AddressBookMethods();
 		 addressBookMethod.Add();
 	}
